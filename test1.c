1a595f98e9a4a9148c09671b006dda17d8e44d4e
shivanikumbhar <58585236+shivanikumbhar@users.noreply.github.com>
2019-12-06 13:12:15 +0530

    Create test1.c

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int man()
+{
+int arr[4];
+int i;
+printf("enter the array elements");
+for(i=0;i<4;i++)
+{
+scanf("%d",&arr[i]);
+}
+printf(" the array elements");
+for(i=0;i<4;i++)
+{
+printf("%d",arr[i]);
+}
+
